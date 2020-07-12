dc2ac1ac6798317c3bcbaf256173d4029d6927da
EdonJung <2donny@naver.com>
2020-07-12 23:40:02 +0900

    weather API

--- weather.js
+++ weather.js
@@ -2,20 +2,27 @@ const API_KEY = "e5d9296e19bf7f7a0e2653e697e1f2fa";
 const  COORDS = 'coords';
 
 
+function getWeather(lat, lon) {
+    fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&APPID=${API_KEY}&units=metric`
+        );
+}
+
 function saveCoords(coordsObj) {
     const coords = JSON.stringify(coordsObj);
-    localStorage.setItem(C햣OORDS, coords);
+    localStorage.setItem(COORDS, coords);
 }
 
 function handleGeoSuccess(position) {
     const latitude = position.coords.latitude;
-    const logitude = position.coords.longitude;
+    const longitude = position.coords.longitude;
     const coordsObj = {
         latitude,
-        logitude,
+        longitude,
     };
 
     saveCoords(coordsObj);
+    getWeather(latitude, longitude);
 }
 
 function handleGEOError() {
@@ -32,7 +39,9 @@ function loadCoords() {
     if(loadedCoords === null) {
         askforCoords();
     }else {
-        // getWeather
+        const parsedCoords = JSON.parse(loadedCoords);
+        console.log(parsedCoords);
+        getWeather(parsedCoords.latitude, parsedCoords.longitude);
         
     }
 }
